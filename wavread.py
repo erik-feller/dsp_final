575a651f6581952edcfdf3a7889fbbb098b8fa6a
erik-feller <erik.feller@colorado.edu>
2015-11-11 17:42:21 +0000

    Adding a function to read wavfiles

---
+++ wavread.py
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# wavread.py
+# Erik Feller
+# 2015-11-11
+#
+# This function will be used to easily read in an audio file which will be used
+# in each of the filters
+###############################################################################
