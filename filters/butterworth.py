9267737916a16456240506b8602538d2abcacf30
erik-feller <erik.feller@colorado.edu>
2015-11-11 18:00:20 +0000

    Added a butterworth filter generating function

---
+++ filters/butterworth.py
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# butterworth.py
+# Erik Feller
+# 2015-11-11
+# a function to create a butterworth filter for the specified stop and pass 
+# bands with lowest order possible. 
+#
+###############################################################################
