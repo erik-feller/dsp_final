1d5980ea228eaabc6588279028612a2f398666bb
erik-feller <erik.feller@colorado.edu>
2015-11-22 16:32:04 +0000

    Significant changes to structure, added ratios for functions

--- harness.py
+++ harness.py
@@ -14,24 +14,24 @@ import scipy.signal as sig
 import scipy.io.wavfile as wav
 import math
 
+#Set variables for the stopband frequencies in case they might change
+leftstop = 1600
+rightstop = 1700
+leftpass = 1400
+rightpass = 1900
+nyquist = 5512.5
+
+#Now change the values into ratios so that SciPy functions can accept them
+stopband_ratio = [(leftstop/nyquist),(rightstop/nyquist)]
+passband_ratio = [(leftpass/nyquist),(rightpass/nyquist)]
+
 #import the dirty audio
 audio_input = wav.read("rawaudio/noisy.wav")
 
 #apply some filters to the audio
+##############################################################################
+# Butterworth Filter
+##############################################################################
 
 
 
-
-
-
-###############################################################################
-#
-# Define a set of functions to design each of the required filters. 
-#
-###############################################################################
-
-#Butterworth 
-def butter_filt(stopband_freq, passband_freq):
-    stopband_rads = stopband_freq/(math.pi*2)
-    passband_rads = passband_freq/(math.pi*2)
-    N, Wn = sig.buttord(
