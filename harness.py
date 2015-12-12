4a0db9cc2181d9fcb28191b707aa1d76bc69ec52
erik-feller <erik.feller@colorado.edu>
2015-12-12 17:18:44 +0000

    About to add in some smoothing and grpdelay

--- harness.py
+++ harness.py
@@ -1,4 +1,4 @@
-###############################################################################
+#############################################################################
 # 
 # This harness should utilize all of the various filters. That are created from
 # the design steps. 
@@ -20,6 +20,8 @@ import matplotlib
 matplotlib.use("Qt4Agg") #force matplotlib to use Qt4
 import matplotlib.pyplot as plt
 
+from grpdelay import group_delay
+import scipy as sp
 import scipy.signal as sig
 import scipy.io.wavfile as wav
 import math
@@ -57,18 +59,20 @@ def output(order, n, d, z, p, k):
     out_file = open('outputs/output.txt', 'a') #open file for output information
     out_file.write(divide_line + test + newline)
     out_file.write("The order of the filter is: " + str(2*N) + newline)
-
     #now work on the graphing
     #start with magnitude and phase graphing
     plt.figure(1)
+    plt.clf()
     ax = plt.axes()
     axes = [ax, ax.twinx()]
     w, h = sig.freqz(n, d, computations)
+    phase = sig.savgol_filter(np.unwrap(np.angle(h)),3,1)
+    #h = sig.savgol_filter(h, 11, 2) #Smooth that data
     axes[0].plot(w/max(w)*nyquist, 20*np.log10(abs(h)),color='Blue') #plotphase
     axes[0].set_ylim(-160, 5)
     axes[0].set_ylabel('Magnitude in dB')
     axes[0].grid(True,linestyle='--')
-    axes[1].plot(w/max(w)*nyquist,np.unwrap(np.arctan2(np.imag(h),np.real(h))), color='Green') #plot phase
+    axes[1].plot(w/max(w)*nyquist, phase, color='Green') #plot phase
     axes[1].set_ylabel('Phase')
     plt.xlim(1300, 2000)
     plt.xlabel('Frequency in Hz')
@@ -76,53 +80,35 @@ def output(order, n, d, z, p, k):
     plt.savefig('outputs/' + test + '_magn_phase.png') #write out image
     #Plot zero-pole
     plt.figure(2)
+    plt.clf()
     plt.plot(np.real(z),np.imag(z),'or') #plot the zeroes
     plt.plot(np.real(p),np.imag(p),'xb') #plot the poles
     t = np.linspace(0,np.pi*2,100) #generate a time interval for unit circle 
     plt.plot(np.cos(t),np.sin(t),'b--')
-    #The long and arduous process of defining the graph axis bounds
-    mins_y = [min(np.imag(z)),min(np.imag(p))]
-    maxs_y = [max(np.imag(z)),max(np.imag(p))]
-    mins_x = [min(np.real(z)),min(np.real(p))]
-    maxs_x = [max(np.real(z)),max(np.real(p))]
-    #Set the x axis
-    if max(maxs_x) > 1:
-        if min(mins_x < -1):
-            plt.xlim(min(mins_x)+0.1,max(maxs_x)+0.1)
-        else:
-            plt.xlim(-1.1, max(maxs_x)+0.1)
-    else:
-        if min(mins_x < -1):
-            plt.xlim(min(mins_x)+0.1,1.1)
-        else:
-            plt.xlim(-1.1, 1.1)
-    #Set the y axis
-    if max(maxs_y) > 1:
-        if min(mins_y < -1):
-            plt.xlim(min(mins_y)+0.1,max(maxs_y)+0.1)
-        else:
-            plt.xlim(-1.1, max(maxs_y)+0.1)
-    else:
-        if min(mins_x < -1):
-            plt.xlim(min(mins_y)+0.1,1.1)
-        else:
-            plt.xlim(-1.1, 1.1)
-
+    plt.xlim(-1.1, 1.1) #Set the x axis
+    plt.ylim(-1.1, 1.1) #Set the y axis
     plt.gca().grid(True, linestyle='--')
     plt.savefig('outputs/' + test + '_pz.png')
     #Plot group delay
     plt.figure(3)
+    plt.clf()
+    angle_h = np.unwrap(np.angle(h))
+    #angle_h = np.angle(n)-np.angle(d)
+    h_diff = (angle_h[0:len(h)-2]-angle_h[2:len(h)])/2
     grpdelay = -np.diff(np.unwrap(np.angle(h)))/np.diff(w) #calculating the group delay using linear derivative approx
-    plt.plot(w[0:len(grpdelay)]/max(w)*nyquist,grpdelay)
+    grpdelay = sig.savgol_filter(grpdelay, 29, 5) #Smooth that data
+    plt.plot((w/max(w)*nyquist)[0:len(grpdelay)],grpdelay)
     plt.gca().grid(True, linestyle='--')
     plt.savefig('outputs/' + test + '_grpdelay.png')
     #Plot impulse response
     plt.figure(4)
+    plt.clf()
     t,(im,) = sig.dimpulse([n,d,1])
     plt.stem(t,im)
     plt.xlim(-1, 100)
     plt.gca().grid(True, linestyle='--')
     plt.savefig('outputs/' + test + '_impulse.png')
+    #Plot the graphs
     if(visual):
         plt.show()
     return 0
@@ -134,7 +120,15 @@ def output(order, n, d, z, p, k):
 # Butterworth Filter
 ##############################################################################
 test = "butterworth"
-N, Wn = sig.buttord(passband_norm,stopband_norm, 3, stop_val, analog=False)
+N, Wn = sig.buttord(passband_norm,stopband_norm, .5, stop_val, analog=False)
 n, d = sig.butter(N, Wn, btype='bandstop',analog=False, output='ba') 
 z, p, k = sig.butter(N, Wn, btype='bandstop',analog=False, output='zpk') 
+output(N, n, d, z, p, k)
+##############################################################################
+# Chebyshev 1
+##############################################################################
+test = "chebyshev1"
+N, Wn = sig.cheb1ord(passband_norm,stopband_norm,.5, stop_val, analog=False)
+n, d = sig.cheby1(N, Wn, btype='bandstop', analog=False, output='ba')
+z, p, k = sig.cheby1(N, Wn, btype='bandstop', analog=False, output='zpk')
 output(N, n, d, z, p, k)
